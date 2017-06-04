36e9528c34a7d4a5a490760d2347d4ae895b4d28
BrunoUQAM <lavigne.bruno.2@courrier.uqam.ca>
2017-06-04 19:27:01 -0400

    code cleanup

--- src/ListeGroupesArrayList.java
+++ src/ListeGroupesArrayList.java
@@ -1,42 +1,27 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
+ * Travail pratique #1
  *
+ * Implémeantation de l'interface {@link IListeGroupes} qui est fourni dans l'énoucé du TP.
  *
- * Created by bruno on 2017-05-26.
+ * @author Bruno Lavigne
+ * Code permanent : LAVB19108108
+ * Courriel : lavigne.bruno.2@courrier.uqam.ca
+ * Cours : INF2120-31
+ * @version 2017-06-04
  */
 public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T> {
 
-    // Attributs d"instances ===============================================================================================
-
     private ArrayList<ArrayList<T>> elements;
     private int nbrElements;
-
-    // Constructeur ========================================================================================================
 
     public ListeGroupesArrayList() {
         setElements(new ArrayList<ArrayList<T>>());
     }
 
-    // Méthodes =============================================================================================================
-
-    /**
-     * Ajoute l'element donne a cette ListeGroupes.
-     *
-     * Si le groupe dont le numero d'identification est element.getId() existe deja
-     * dans cette listeGroupes, l'element est ajoute A LA FIN du groupe existant
-     * SI ET SEULEMENT S'IL n'existe aucun element de ce groupe etant egal a
-     * l'element donne. Autrement, l'element donne n'est pas ajoute.
-     *
-     * Si le groupe element.getId() n'existe pas dans cette listeGroupes, un nouveau
-     * groupe est ajoute (A LA FIN de cette listeGroupes) et l'element donne devient
-     * le premier element du nouveau groupe element.getId().
-     *
-     * @param element l'element a ajouter a cette listeGroupes.
-     * @return true si l'element donne est ajoute, false sinon.
-     * @throws NullPointerException si l'element donne est null.
-     */
     @Override
     public boolean ajouter(T element) {
         boolean isAjoute = false;
@@ -50,7 +35,7 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
             final int groupePosition = trouveGroupeIndex(element.getId());
 
             if (!elementExiste(element)) {
-                getElements().get(groupePosition).add(0, element);
+                getElements().get(groupePosition).add(element);
                 setNbrElements(getNbrElements() + 1);
                 isAjoute = true;
             }
@@ -68,45 +53,26 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
         return isAjoute;
     }
 
-    /**
-     * Supprime de cette listeGroupes le groupe dont l'id est l'idGroupe donne.
-     * Si le groupe ayant l'idGroupe donne n'existe pas dans cette listeGroupes,
-     * celle-ci demeure inchangee.
-     *
-     * @param idGroupe le numero d'identification du groupe a supprimer.
-     * @return le nombre d'elements dans le groupe supprime ou 0 si la suppression n'a pas lieu.
-     */
     @Override
     public int supprimerGroupe(int idGroupe) {
 
+        int nbGroupElement = 0;
+
         if (groupeExiste(idGroupe)) {
             final int positionGroupe = trouveGroupeIndex(idGroupe);
-            int nbGroupElement = getElements().get(positionGroupe).size();
+
+            nbGroupElement = getElements().get(positionGroupe).size();
             setNbrElements(getNbrElements() - nbGroupElement);
             getElements().remove(positionGroupe);
-
-            return nbGroupElement;
         }
 
-        return 0;
+        return nbGroupElement;
     }
 
-    /**
-     * Supprime l'element donne de son groupe d'appartenance.
-     * <p>
-     * Si apres suppression de l'element le groupe est vide, il est supprime
-     * de cette listeGroupes.
-     * <p>
-     * Si le groupe element.getId() n'existe pas dans cette listeGroupes ou
-     * si l'element donne n'est pas dans le groupe element.getId(), cette
-     * listeGroupes demeure inchangee.
-     *
-     * @param element l'element a supprimer de cette listeGroupes.
-     * @return true si l'element est supprime, false sinon.
-     * @throws NullPointerException si element est null.
-     */
     @Override
     public boolean supprimerElement(T element) {
+        boolean elementSupprimer = false;
+
         if (element == null) {
             throw new NullPointerException();
         }
@@ -116,31 +82,20 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
                 final int groupePosition = trouveGroupeIndex(element.getId());
                 final int elementPosition = trouveElementIdex(element);
                 getElements().get(groupePosition).remove(elementPosition);
-                this.nbrElements -= 1;
+                setNbrElements(getNbrElements() -1);
 
                 if (getElements().get(groupePosition).isEmpty()) {
                     getElements().remove(groupePosition);
                 }
 
-                return true;
+                elementSupprimer = true;
+
             }
         }
 
-        return false;
+        return elementSupprimer;
     }
 
-    /**
-     * Permet d'obtenir le numero d'identification (l'id) du groupe ayant la
-     * postion donnee dans cette listeGroupes, si cette position est valide.
-     * Une position valide doit etre comprise entre 0 et nbrGroupes() - 1
-     * inclusivement.
-     *
-     * @param position la position, dans cette listeGroupes, du groupe dont on
-     *                 veut obtenir l'id.
-     * @return l'id du groupe ayant la position donnee.
-     * @throws NoSuchElementException si la position donnee n'est pas une position
-     *                                valide dans cette listeGroupes.
-     */
     @Override
     public int obtenirIdGroupe(int position) {
 
@@ -151,21 +106,6 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
         return getElements().get(position).get(0).getId();
     }
 
-    /**
-     * Permet d'obtenir l'element qui se trouve a la position donnee, dans le
-     * groupe dont l'id est l'idGroupe donne, si la position donnee est valide.
-     * Une position valide doit etre comprise entre 0 et taille(idGroupe) - 1
-     * inclusivement.
-     *
-     * @param idGroupe l'id du groupe dans lequel rechercher l'element a retourner.
-     * @param position la postition de l'element a retourner, dans le groupe dont
-     *                 l'id est l'idGroupe donne.
-     * @return l'element a la position donnee, dans le groupe dont l'id est
-     * l'idGroupe donne.
-     * @throws NoSuchElementException si le groupe ayant l'idGroupe donne n'existe
-     *                                pas dans cette listeGroupe ou si la position donnee n'est pas un
-     *                                indice valide dans le groupe dont l'id est l'idGroupe donne.
-     */
     @Override
     public T obtenirElement(int idGroupe, int position) {
         if (!groupeExiste(idGroupe)) {
@@ -180,17 +120,12 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
         return getElements().get(positionGroup).get(position);
     }
 
-    /**
-     * Permet d'obtenir le nombre d'elements presents dans cette listeGroupes.
-     *
-     * @return le nombre d'elements presents dans cette listeGroupes.
-     */
     @Override
     public int taille() {
         int nbElements = 0;
 
-        for (int i = 0; i < this.elements.size(); i++) {
-            for (int j = 0; j < this.elements.get(i).size(); j++) {
+        for (int i = 0; i < getElements().size(); i++) {
+            for (int j = 0; j < getElements().get(i).size(); j++) {
                 nbElements += 1;
             }
         }
@@ -198,19 +133,15 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
         return nbElements;
     }
 
-    /**
-     * Permet d'obtenir le nombre d'elements presents dans le groupe ayant
-     * comme numero d'identification l'idGroupe donne.
-     *
-     * @param idGroupe le numero d'identification du groupe dont on veut
-     *                 le nombre d'elements.
-     * @return le nombre d'elements dans le groupe ayant l'idGroupe donne ou
-     *         -1 si l'idGroupe donne ne correspond a aucun groupe dans cette
-     *         listeGroupes.
-     */
     @Override
     public int taille(int idGroupe) {
-        return groupeExiste(idGroupe) ? this.elements.get(idGroupe).size() : -1;
+        int taille = -1;
+
+        if (groupeExiste(idGroupe)) {
+            taille = getElements().get(idGroupe).size();
+        }
+
+        return taille;
     }
 
     /**
@@ -220,7 +151,7 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
      */
     @Override
     public int nbrGroupes() {
-        return this.elements.size();
+        return getElements().size();
     }
 
     /**
@@ -232,13 +163,17 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
     @Override
     public boolean groupeExiste(int idGroupe) {
 
-        for (ArrayList<T> group : this.elements) {
+        boolean groupeExiste = false;
+
+        final Iterator<ArrayList<T>> iterator = getElements().iterator();
+        while(iterator.hasNext() && !groupeExiste) {
+            ArrayList<T> group = iterator.next();
             if (group.get(0).getId() == idGroupe) {
-                return true;
+                groupeExiste = true;
             }
         }
 
-        return false;
+        return groupeExiste;
     }
 
     /**
@@ -253,91 +188,56 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
      */
     @Override
     public boolean elementExiste(T element) {
+        boolean elementExiste = false;
+
         if (element == null) {
             throw new NullPointerException();
         }
 
         if (groupeExiste(element.getId())) {
-            for (T groupe : this.elements.get(trouveGroupeIndex(element.getId()))) {
+            final int groupeIndex = trouveGroupeIndex(element.getId());
+            Iterator<T> iterator = getElements().get(groupeIndex).iterator();
+
+            while (iterator.hasNext() && !elementExiste) {
+                T groupe = iterator.next();
                 if (groupe.equals(element)) {
-                    return true;
+                    elementExiste = true;
                 }
             }
         }
 
-        return false;
+        return elementExiste;
     }
 
-    /**
-     * Transfere les elements du groupe dont le numero d'identification est
-     * idGroupe2 dans le groupe ayant le numero d'identification idGroupe1, en
-     * respectant les regles donnees ci-dessous.
-     * <p>
-     * 1. Les deux groupes donnes (celui ayant l'idGroupe1 et celui ayant
-     * l'idGroupe2) doivent exister dans cette listeGroupes pour que le
-     * transfert ait lieu.
-     * <p>
-     * 2. Les elements du groupe ayant l'idGroupe2 doivent etre transferes DANS
-     * L'ORDRE (du premier au dernier), A LA FIN du groupe ayant l'idGroupe1.
-     * <p>
-     * 3. L'appel de element.getId() sur chaque element transfere doit retourner
-     * idGroupe1 (et non plus idGroupe2) apres le transfert (les elements
-     * transferes doivent changer leur idGroupe pour celui du groupe
-     * dans lequel ils sont transferes).
-     * <p>
-     * 4. Apres le transfert, il ne doit y avoir aucun doublon dans le groupe
-     * ayant l'idGroupe1 (on ne transfere pas les doublons).
-     * <p>
-     * 5. Apres le transfert, le groupe ayant l'idGroupe2 n'existe plus dans cette
-     * listeGroupes.
-     *
-     * @param idGroupe1 le numero d'identification du groupe qui recevra les
-     *                  elements du groupe ayant l'idGroupe2.
-     * @param idGroupe2 le numero d'identification du groupe dont on veut
-     *                  transferer les elements dans le groupe ayant l'idGroupe1.
-     * @return le nombre d'elements du groupe ayant l'idGroupe2 qui ont ete
-     * transferes dans le groupe ayant l'idGroupe1.
-     */
     @Override
     public int transferer(int idGroupe1, int idGroupe2) {
-
-        // TODO : not working like this
 
         if (groupeExiste(idGroupe1) && groupeExiste(idGroupe2)) {
             final int posGroup1 = trouveGroupeIndex(idGroupe1);
             final int posGroup2 = trouveGroupeIndex(idGroupe2);
 
-            for (T elementGroup2 : this.elements.get(posGroup2)) {
-                for (T elementGroup1 : this.elements.get(posGroup1)) {
-                    if (!elementGroup2.equals(elementGroup1)) {
-                        elementGroup2.setId(idGroupe1);
-                        this.elements.get(posGroup1).add(elementGroup2);
-                    }
+            ArrayList<T> listeTemporaire = new ArrayList<>();
+
+            for (T elementGroup2 : getElements().get(posGroup2)) {
+                if (!elementExistDansGroup(idGroupe1, elementGroup2)) {
+                    elementGroup2.setId(idGroupe1);
+                    listeTemporaire.add(elementGroup2);
                 }
             }
 
+            getElements().get(posGroup1).addAll(listeTemporaire);
             getElements().remove(posGroup2);
+
+            return listeTemporaire.size();
         }
 
         return 0;
     }
 
-    /**
-     * Remplace l'element1 de cette listeGroupes par l'element2 donne.
-     *
-     * - Si element1 n'existe pas dans cette listeGroupes, le remplacement n'a
-     *   pas lieu.
-     *
-     * - Si l'id de element1 est different de l'id de l'element2, le remplacement
-     *   n'a pas lieu.
-     *
-     * @param element1 l'element a remplacer par element2.
-     * @param element2 l'element qui remplace l'element1.
-     * @return true si le remplacement a eu lieu, false sinon.
-     * @throws NullPointerException si element1 ou element2 est null.
-     */
     @Override
     public boolean remplacer(T element1, T element2) {
+        boolean remplacer = false;
+
         if (element1 == null || element2 == null) {
             throw new NullPointerException();
         }
@@ -346,25 +246,28 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
             if (element1.getId() == element2.getId()) {
                 final int posGroup1 = trouveGroupeIndex(element1.getId());
                 getElements().get(posGroup1).set(trouveElementIdex(element1), element2);
-                return true;
+                remplacer = true;
             }
         }
-        return false;
+        return remplacer;
     }
 
-    // Méthodes ajoutés qui ne font pas parties de l'interface ===============================================================
-
     private boolean elementExistDansGroup(int idGroup, T element) {
+        boolean elementExist = false;
 
         if (groupeExiste(idGroup)) {
-            for (T groupe : this.elements.get(trouveGroupeIndex(idGroup))) {
+            final int groupeIndex = trouveGroupeIndex(idGroup);
+            final Iterator<T> iterator = getElements().get(groupeIndex).iterator();
+
+            while(iterator.hasNext() && !elementExist) {
+                T groupe = iterator.next();
                 if (groupe.equals(element)) {
-                    return true;
+                    elementExist = true;
                 }
             }
         }
 
-        return false;
+        return elementExist;
     }
 
     /**
@@ -374,14 +277,22 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
      * @return L'index si le groupe est trouvé, -1 sinon.
      */
     private int trouveGroupeIndex(int idGroupe) {
+        int groupeIndex = -1;
+        boolean groupeTrouve = false;
 
-        for (int i = 0; i < this.elements.size(); i++) {
-            if (this.elements.get(i).get(0).getId() == idGroupe) {
-                return i;
+        Iterator<ArrayList<T>> iterator = getElements().iterator();
+        int i = 0;
+
+        while (iterator.hasNext() && !groupeTrouve) {
+            ArrayList<T> groups = iterator.next();
+            if (groups.get(0).getId() == idGroupe) {
+                groupeTrouve = true;
+                groupeIndex = i;
             }
+            i++;
         }
 
-        return -1;
+        return groupeIndex;
     }
 
     /**
@@ -392,21 +303,23 @@ public class ListeGroupesArrayList<T extends IGroupe> implements IListeGroupes<T
      * @return L'index de l'élément s'il est trouvé, -1 sinon
      */
     private int trouveElementIdex(T element) {
-
+        int elementIndex = -1;
+        boolean groupeTrouve = false;
         final int groupePos = trouveGroupeIndex(element.getId());
 
         int i = 0;
-        for (T groupeElement : getElements().get(groupePos)) {
+        final Iterator<T> iterator = getElements().get(groupePos).iterator();
+        while (iterator.hasNext() && !groupeTrouve) {
+            T groupeElement = iterator.next();
             if (groupeElement.equals(element)) {
-                return i;
+                groupeTrouve = true;
+                elementIndex = i;
             }
             i++;
         }
 
-        return -1;
+        return elementIndex;
     }
-
-    // Getters et Setters ====================================================================================================
 
     private ArrayList<ArrayList<T>> getElements() {
         return elements;
--- src/testeurListe.java
+++ src/testeurListe.java
@@ -6,9 +6,11 @@
 public class testeurListe {
 
     public static void main(String [] arg) {
-        //test1();
-        test2();
+        test1();
+        //test2();
         //test3();
+        //testTransfert();
+        //testRemplacer();
     }
 
     private static void test1() {
@@ -22,10 +24,10 @@ public class testeurListe {
         liste.ajouter(kim);
         liste.ajouter(max);
         liste.ajouter(bob);
-        liste.ajouter(bob);
+        liste.ajouter(joe);
 
         System.out.println("Taille de la liste : " + liste.taille());
-        System.out.println("Max existe ? : " + liste.elementExiste(bob));
+        System.out.println("Max existe ? : " + liste.elementExiste(joe));
     }
 
     private static void test2() {
@@ -53,11 +55,13 @@ public class testeurListe {
         liste.ajouter(e4_2);
         liste.ajouter(e8);
 
+        final int pos = liste.obtenirIdGroupe(2);
+
         //liste.supprimerElement(e3);
         //liste.supprimerElement(e6);
 
-        //liste.supprimerGroupe(7);
-        //liste.supprimerGroupe(12);
+        liste.supprimerGroupe(7);
+        liste.supprimerGroupe(12);
 
         liste.transferer(3, 7);
     }
@@ -79,5 +83,54 @@ public class testeurListe {
         } catch (NullPointerException e) {
             System.out.println("OK");
         }
+    }
+
+    private static void testTransfert() {
+        AbonneRevue e1      = new AbonneRevue(3, "e1");
+        AbonneRevue e2      = new AbonneRevue(3, "e2");
+        AbonneRevue e3      = new AbonneRevue(3, "e3");
+        AbonneRevue e4      = new AbonneRevue(3, "e4");
+
+        AbonneRevue e7      = new AbonneRevue(5, "e7");
+        AbonneRevue e3_2    = new AbonneRevue(5, "e3");
+        AbonneRevue e5      = new AbonneRevue(5, "e5");
+        AbonneRevue e6      = new AbonneRevue(5, "e6");
+        AbonneRevue e1_2    = new AbonneRevue(5, "e1");
+
+        IListeGroupes<AbonneRevue> liste = new ListeGroupesArrayList<>();
+
+        liste.ajouter(e1);
+        liste.ajouter(e2);
+        liste.ajouter(e3);
+        liste.ajouter(e4);
+
+        liste.ajouter(e7);
+        liste.ajouter(e3_2);
+        liste.ajouter(e5);
+        liste.ajouter(e6);
+        liste.ajouter(e1_2);
+
+        liste.transferer(3,5);
+
+    }
+
+    private static void testRemplacer() {
+
+        AbonneRevue e1      = new AbonneRevue(3, "e1");
+        AbonneRevue e2      = new AbonneRevue(3, "e2");
+        AbonneRevue e3      = new AbonneRevue(3, "e3");
+        AbonneRevue e4      = new AbonneRevue(3, "e4");
+        AbonneRevue e5      = new AbonneRevue(3, "e5");
+
+
+        IListeGroupes<AbonneRevue> liste = new ListeGroupesArrayList<>();
+
+        liste.ajouter(e1);
+        liste.ajouter(e2);
+        liste.ajouter(e3);
+        liste.ajouter(e4);
+
+        final boolean remplacer = liste.remplacer(e1, e5);
+
     }
 }
